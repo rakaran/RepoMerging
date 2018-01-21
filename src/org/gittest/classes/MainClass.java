706bec394be470c299ffe5a8125c254944c854f3
Ramesh Karan <rk.karn32@gmail.com>
2018-01-21 15:33:58 -0500

    Main class added

---
+++ src/org/gittest/classes/MainClass.java
@@ -0,0 +1,12 @@
+package org.gittest.classes;
+
+public class MainClass {
+	public static void main(String[] args) {
+		MainClass mainC = new MainClass();
+		mainC.methodRamesh();
+	}
+	
+	public void methodRamesh(){
+		System.out.println("Method created by User1");
+	}
+}
