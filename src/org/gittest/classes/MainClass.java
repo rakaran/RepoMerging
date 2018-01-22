1604e280de22e3f054041dcc5ccc758ea783a28e
Ramesh Karan <rk.karn32@gmail.com>
2018-01-21 20:09:44 -0500

    Added new method For other user

--- src/org/gittest/classes/MainClass.java
+++ src/org/gittest/classes/MainClass.java
@@ -1,12 +1,25 @@
 package org.gittest.classes;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class MainClass {
 	public static void main(String[] args) {
 		MainClass mainC = new MainClass();
 		mainC.methodRamesh();
+		mainC.methodOfElse();
 	}
 	
 	public void methodRamesh(){
 		System.out.println("Method created by User1");
+	}
+	public void methodOfElse(){
+		String user = null ;
+		try {
+			user = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Method created by:"+user);
 	}
 }
